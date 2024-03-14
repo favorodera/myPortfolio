7ad104814f863577b8845ec30dab5ab4431de6cd
favorodera <favorodera@gmail.com>
2024-03-14 10:04:10 +0000

    nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 `strict`
+
+// Turns the inputed value of the nav search input to uppercase
+function navSearchToUpperCase() {
+    let navSearch = document.querySelector(`#navSearch`);
+    navSearch.value = navSearch.value.toUpperCase();
+};
